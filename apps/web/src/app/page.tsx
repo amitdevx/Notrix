36492e6636cd505d7d33c84842fe6587871ff8ff
amitdevx <iaminfinity.gamer123@gmail.com>
2026-06-23 23:03:53 +0530

    chore: add bg-background to Home page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,7 +2,7 @@ import { Workspace } from '../components/Workspace';
 
 export default function Home() {
   return (
-    <main className="h-screen w-screen overflow-hidden">
+    <main className="h-screen w-screen overflow-hidden bg-background">
       <Workspace />
     </main>
   );
